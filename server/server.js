9bb5c6324db1259e8331512301e257905bc2d5a1
Seth <45956512+projectLewis@users.noreply.github.com>
2019-10-18 15:50:56 -0500

    Server client merger (#18)
    * merged
    
    * minor tweaks for production

--- server/server.js
+++ server/server.js
@@ -1,14 +1,51 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname + "/../build/")));
 
-let port = 3001;
+const PORT = 3001;
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
+const db = require("../database/db");
+
+app.get("/api/allproducts", (req, res) => {
+  db.findAllProducts()
+    .then(product => res.send(product))
+    .catch(err => {
+      res.status(400).send("couldnt return those records");
+    });
+});
+
+app.get("/api/moreshopproducts", (req, res) => {
+  db.findShopProducts()
+    .then(product => res.send(product))
+    .catch(err => {
+      res.status(400).send("couldnt return those records");
+    });
+});
+
+app.get("/api/product/:listing_id", (req, res) => {
+  const listingID = req.params;
+  db.findCombinedProductsforID(listingID)
+    .then(product => res.send(product))
+    .catch(err => {
+      res.status(400).send("couldnt find that record");
+    });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../build/index.html"));
+});
+
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
 });
--- src/Const.ts
+++ src/Const.ts
@@ -1,4 +1,4 @@
 export const APIENDPOINT =
-  "http://ec2-3-14-146-35.us-east-2.compute.amazonaws.com";
+  "http://ec2-3-14-146-35.us-east-2.compute.amazonaws.com/api";
 
-// export const APIENDPOINT = "http://localhost:3001";
+// export const APIENDPOINT = "http://localhost:3001/api";
